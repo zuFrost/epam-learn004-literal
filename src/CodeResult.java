dc2270f284c5e4eadcc2c2f8940fc10ec917c2a8
Alex Frost <zufrost.a@gmail.com>
2020-04-14 16:28:21 +0300

    add content of lection and check questions

---
+++ src/CodeResult.java
@@ -0,0 +1,14 @@
+public class CodeResult {
+    public static void main(String args[]){
+        Integer number = 32;
+        // Binary number format
+        String convert = Integer.toBinaryString(number);
+        System.out.print(convert + " ");
+        // Octagonal number format
+        convert = Integer.toOctalString(number);
+        System.out.print(convert + " ");
+        // Hexadecimal number format
+        convert = Integer.toHexString(number).toUpperCase();
+        System.out.print(convert + " ");
+    }
+}
---
+++ src/QuestionCheck.java
@@ -0,0 +1,60 @@
+public class QuestionCheck {
+    public static void main(String[] args) {
+        // Укажите 3 строки, компиляция которых не приведет к ошибке
+//        int var1 = 356f;
+
+        double var2 = 356f;
+
+        float var3 = 356f;
+
+//        char var4 = 356f;
+
+//        long var5 = 356f;
+
+//        byte var6 = 356f;
+
+//        Integer var7 = 356f;
+
+//        Character var8 = 356f;
+
+        Object var9 = 356f;
+
+        // Какие из следующих строк скомпилируются без ошибки? Выберите 2
+
+//        float f = 7.0;
+
+//        char c = "z";
+
+//        byte b = 254;
+
+//        boolean n = null;
+
+        int i = 32565;
+
+        int j = 'ъ';
+        System.out.println(j);
+
+        //Какие из следующих строк скомпилируются без ошибки? Выберите 2
+
+        char c = 17;
+
+//        char ch = '10';
+
+//        byte b = 255;
+
+        int i1 = 's';
+
+//        boolean n = null;
+
+//        float = 7.0f;
+
+
+
+
+
+
+
+
+
+    }
+}
